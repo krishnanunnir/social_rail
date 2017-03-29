935421724615c183672baf176cc104f31a8f75fb
Krishnanunni <krishnanunnir97@gmail.com>
2017-03-29 15:24:26 +0530

    Added a full title helper

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,9 @@
 module StaticPagesHelper
+  def fulltitle( pagetitle="")
+    if pagetitle.empty?
+      return "Ruby on Rails"
+    else
+      return pagetitle + " | Ruby on Rails"
+    end
+  end
 end
